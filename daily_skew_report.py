34b63223654ef058e295b8bca29501c322d533ae
cassrobson <casselrobsongithub@gmail.com>
2025-02-13 17:16:46 -0500

    ready

--- daily_skew_report.py
+++ daily_skew_report.py
@@ -46,26 +46,38 @@ def calculate_call_delta(S, K, T, r, sigma):
     delta = stats.norm.cdf(d1)
     return delta
 
+def calculate_put_delta(S, K, T, r, sigma):
+    # Calculate d1
+    d1 = (math.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * math.sqrt(T))
+    
+    # Put delta is call delta minus 1
+    delta = stats.norm.cdf(d1) - 1
+    return delta
+
 # Function to calculate SPX skew
 def calculate_spx_skew(calls, puts, spot, T):
     spotr = round(spot, 0)
     calls = calls[(calls['strike'] <= spotr+100)&(calls['strike']>= spotr-100)]
     puts = puts[(puts['strike'] <= spotr+100)&(puts['strike']>= spotr-100)]
     calls['delta'] = calls.apply(lambda row: calculate_call_delta(spot, row['strike'], T, 0, row['impliedVolatility']), axis=1)
-    puts['delta'] = puts.apply(lambda row: calculate_call_delta(spot, row['strike'], T, 0, row['impliedVolatility']), axis=1)
+    puts['delta'] = puts.apply(lambda row: calculate_put_delta(spot, row['strike'], T, 0, row['impliedVolatility']), axis=1)
 
+    
     calls = calls[(calls['delta']>=0.25)&(calls['delta']<=0.3)].head(1).reset_index()
-    puts = puts[(puts['delta']>=0.25)&(puts['delta']<=0.3)].head(1).reset_index()
-
+    puts = puts[(puts['delta']<=-0.25)&(puts['delta']>=-0.3)].tail(1).reset_index()
     call_30d_iv = calls.loc[0, "impliedVolatility"]
+    call_delta = calls.loc[0, "delta"]
+    call_strike = calls.loc[0, 'strike']
     put_30d_iv = puts.loc[0, "impliedVolatility"]
+    put_delta = puts.loc[0, "delta"]
+    put_strike = puts.loc[0, 'strike']
     skew = put_30d_iv-call_30d_iv
 
-    return call_30d_iv, put_30d_iv, skew
+    return call_30d_iv, put_30d_iv, skew, call_delta, call_strike, put_delta, put_strike
 
 def plot():
     plot_skew = pd.read_csv("spx_skew.csv")
-    plot_skew.columns = ['Date', '30Δ Call IV', '30Δ Put IV', 'Skew']
+    plot_skew.columns = ['Date', '30Δ Call IV', 'Call Delta', "Call Strike", '30Δ Put IV', "Put Delta", 'Put Strike', 'Skew']
     df = plot_skew
     df["Date"] = pd.to_datetime(df["Date"])
 
@@ -96,7 +108,13 @@ def plot():
     # Save plot
     plt.savefig("plots/iv_skew_plot.png", bbox_inches="tight")
     plt.close()
-    return plot_skew
+
+    plot_skew = plot_skew.sort_values("Date", ascending=False)
+    plot_skew['Change'] = plot_skew["Skew"]-plot_skew['Skew'].shift(-1)
+    plot_skew["Signal"] = plot_skew["Change"].apply(lambda x: "Bearish" if x > 0 else "Bullish")
+    today_signal = plot_skew.loc[len(plot_skew)-1, "Signal"]
+
+    return plot_skew, today_signal
 
 
 # Function to track daily SPX skew
@@ -117,26 +135,25 @@ def track_daily_spx_skew():
     time_to_exp = time_to_exp_sec / (252*24*60*60)
 
     # Calculate SPX skew
-    call_30d_iv, put_30d_iv, skew = calculate_spx_skew(calls, puts, spot, time_to_exp)
+    call_30d_iv, put_30d_iv, skew, call_delta, call_strike, put_delta, put_strike = calculate_spx_skew(calls, puts, spot, time_to_exp)
     
     # Store the result in a DataFrame
-    skew_df = pd.DataFrame({'Date': [today], '30Δ Call IV':[call_30d_iv], '30Δ Put IV':[put_30d_iv], 'SPX_Skew': [skew]})
-  
+    skew_df = pd.DataFrame({'Date': [today], 'Call IV':[call_30d_iv],'Call Delta':[call_delta], 'Call Strike':[call_strike],'Put IV':[put_30d_iv],'Put Delta':[put_delta],'Put Strike':[put_strike], 'SPX Skew': [skew]})
+
     # Save to CSV (append mode)
     try:
         skew_df.to_csv('spx_skew.csv', mode='a', header=False, index=False)
     except FileNotFoundError:
         skew_df.to_csv('spx_skew.csv', mode='w', header=True, index=False)
 
-    df = plot()
+    df, signal = plot()
     
 
-    print(f"SPX Skew on {today}: {skew}")
-    return df
+    return df.head(5), signal
 
 # Run the script daily
 if __name__ == "__main__":
-    df = track_daily_spx_skew()
+    df, signal = track_daily_spx_skew()
     plot_path = "plots/iv_skew_plot.png"  # Ensure this path is correct
 
     # Email configuration
@@ -145,7 +162,7 @@ if __name__ == "__main__":
     SENDER_EMAIL = "casselrobson93@gmail.com"
     SENDER_PASSWORD = "lajhhtqevwvomcts"  # Use an app password if using Gmail
     RECIPIENT_EMAIL = "casselrobson19@gmail.com"
-    SUBJECT = f"Daily Report - {datetime.today().strftime('%Y-%m-%d')}"
+    SUBJECT = f"Daily Skew Sentiment {signal} - {datetime.today().strftime('%Y-%m-%d')}"
 
     # Convert DataFrame to HTML table
     df_html = df.to_html(index=False, border=1, justify="center")
@@ -167,9 +184,9 @@ if __name__ == "__main__":
         </style>
     </head>
     <body>
-        <h2>Daily Data Report</h2>
+        <h2>Daily Skew Report (5D)</h2>
         {df_html}
-        <p>Attached is the latest plot.</p>
+        <p>Plot of Raw and Rolling Skew.</p>
     </body>
     </html>
     """
